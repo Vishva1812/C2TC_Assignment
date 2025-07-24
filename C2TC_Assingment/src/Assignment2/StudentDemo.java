d51e0a30f38d0670892a443649dcdaa617ab2070
Dell <Dell@DESKTOP-UDAHKSB>
2025-07-24 11:04:20 +0530

    Assignment Program

---
+++ C2TC_Assingment/src/Assignment2/StudentDemo.java
@@ -0,0 +1,12 @@
+
+package Assignment2;
+
+public class StudentDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Student s1 = new Student("vishva", 20, "IT");
+
+	        // Calling method
+		 s1.displayDetails();
+}
+}
